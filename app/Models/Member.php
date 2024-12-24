4920dc350ee0d87982f5bd63175cb1a553e3a989
michael <michaelrio9966@gmail.com>
2024-12-24 19:23:29 +0700

    push

--- app/Models/Member.php
+++ app/Models/Member.php
@@ -2,9 +2,15 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Member extends Model
 {
-    //
+    use HasFactory;
+    protected $table = 'member';
+    protected $primaryKey = 'id_member';
+    protected $keyType = 'string'; 
+    protected $fillable = ['id_member', 'nama', 'no_telp', 'alamat', 'tanggal_lahir', 'mulai_member', 'akhir_member'];
+    
 }
