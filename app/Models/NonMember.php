c678e4cd3df3174c0431c604b09bb005afc8b362
michael <michaelrio9966@gmail.com>
2024-12-24 19:24:35 +0700

    push

--- app/Models/NonMember.php
+++ app/Models/NonMember.php
@@ -2,9 +2,16 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class NonMember extends Model
 {
-    //
+    use HasFactory;
+    protected $table = 'non_member';
+    protected $fillable = ['id', 'nama', 'no_telp'];
+    public function Invoice()
+    {
+        return $this->belongsTo(Invoice::class,'id','id_player');
+    }
 }
--- app/Models/Order.php
+++ app/Models/Order.php
@@ -2,9 +2,21 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    //
+    use HasFactory;
+    protected $fillable = ['id','id_table','status'];
+
+    public function items()
+    {
+        return $this->hasMany(OrderItem::class, 'order_id', 'id');
+    }
+
+    public function invoice()
+    {
+        return $this->belongsTo(Invoice::class, 'id_belanja', 'id_table');
+    }
 }
