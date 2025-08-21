2158e01d71c9685d12b4ecd180c873b1ba3c7619
michael <michaelrio9966@gmail.com>
2025-08-21 22:02:51 +0700

    update

--- app/Http/Controllers/BilliardController.php
+++ app/Http/Controllers/BilliardController.php
@@ -554,7 +554,7 @@ class BilliardController extends Controller
         $payload = [
             'nomor_meja' => $request->no_meja,
             'status' => 'lanjut',
-            'start_time' => $b->waktu_mulai
+            'start_time' => $tanggalMain->toDateTimeString()
         ];
         try {
             Http::post('http://127.0.0.1:3001/meja/update', $payload);
